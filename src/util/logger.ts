ebcc4df46656333c73481b1edba368bd5a2e84c5
Yucel Okcu <yucel.n.okcu@gmail.com>
2021-03-25 16:06:57 +0300

    Fix logs

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,11 +1,18 @@
 // eslint-disable-next-line import/named
 import { Logger } from "@zeplin/cli";
 
-let loggerInstance: Logger;
+let loggerInstance: Logger = {
+    error: console.error,
+    info: console.info,
+    warn: console.warn,
+    debug: console.debug
+};
 
-const getLogger = (): Logger | undefined => loggerInstance;
-const setLogger = (logger: Logger): void => {
-    loggerInstance = logger;
+const getLogger = (): Logger => loggerInstance;
+const setLogger = (logger?: Logger): void => {
+    if (logger) {
+        loggerInstance = logger;
+    }
 };
 
 export { getLogger, setLogger };
